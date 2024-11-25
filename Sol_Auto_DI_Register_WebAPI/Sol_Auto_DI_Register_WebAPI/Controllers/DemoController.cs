6e03abd4bc195a5a69a63d9030d8fcc7c9455c8b
Kishor Naik <kishor.naik011.net@gmail.com>
2024-11-25 09:44:49 +0530

    @KishorNaik: Init

---
+++ Sol_Auto_DI_Register_WebAPI/Sol_Auto_DI_Register_WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Auto_DI_Register_WebAPI.Services;
+
+namespace Sol_Auto_DI_Register_WebAPI.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly IScopedDemo _scopedDemo;
+
+        public DemoController(IScopedDemo scopedDemo)
+        {
+            _scopedDemo = scopedDemo;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _scopedDemo.DoSomething();
+            return "Hello World";
+        }
+    }
+}
---
+++ Sol_Auto_DI_Register_WebAPI/Sol_Auto_DI_Register_WebAPI/Services/DemoService.cs
@@ -0,0 +1,45 @@
+﻿using AutoDependencyRegistration.Attributes;
+
+namespace Sol_Auto_DI_Register_WebAPI.Services;
+
+public interface ISingletonDemo
+{
+    void DoSomething();
+}
+
+[RegisterClassAsSingleton]
+public class SingletonDemo : ISingletonDemo
+{
+    void ISingletonDemo.DoSomething()
+    {
+        Console.WriteLine("SingletonDemo");
+    }
+}
+
+public interface IScopedDemo
+{
+    void DoSomething();
+}
+
+[RegisterClassAsScoped]
+public class ScopedDemo : IScopedDemo
+{
+    void IScopedDemo.DoSomething()
+    {
+        Console.WriteLine("ScopedDemo");
+    }
+}
+
+public interface ITransientDemo
+{
+    void DoSomething();
+}
+
+[RegisterClassAsTransient]
+public class TransientDemo : ITransientDemo
+{
+    void ITransientDemo.DoSomething()
+    {
+        throw new NotImplementedException();
+    }
+}
